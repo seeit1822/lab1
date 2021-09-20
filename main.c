d9cbd480ac96e67832f8cfc5bc7582df822b262d
Evgenij <seeit1822@ya.ru>
2021-09-20 20:01:05 +0300

    Add .gitignore
    Add file saving functionality

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#define OUTPUT "Some text."
+
 int main()
 {
 	puts("Welcome, user!");
-}
 
+	char *output = OUTPUT;
+	FILE *output_file = fopen("output.txt", "wb+");
+	fwrite(output_file, 1, sizeof(OUTPUT), output_file);
+	fclose(output_file);
+}
